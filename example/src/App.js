1a408709613df210c5d56f72cfdc7b099c21424d
Josh Wilkerson <jgwilkerson@gmail.com>
2019-10-05 22:47:58 -0500

    initial create-react-library setup

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'simpli'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <ExampleComponent text='Modern React component module' />
+      </div>
+    )
+  }
+}
