88521de7960c1520c296785d24d00244bbc92ee7
Josh Wilkerson <jgwilkerson@gmail.com>
2019-12-12 12:06:25 -0600

    updates to button component props

--- src/button.js
+++ src/button.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import { string } from 'prop-types'
+import { string, any, func } from 'prop-types'
+import styled from '@emotion/styled'
 
-const Button = props => {
-  return <button>{props.text}</button>
+const Button = ({ children, text = 'button text', onClick }) => {
+  const Button = styled.button`
+    background: #333;
+    color: #fff;
+    outline: none;
+    padding: 8px 12px;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: .2s all ease;
+
+    &:hover {
+      background: #666;
+    }
+  `
+
+  return <Button onClick={onClick}>{children || text}</Button>
 }
 
 Button.propTypes = {
-  text: string
-}
-
-Button.defaultProps = {
-  text: 'button'
+  text: string.isRequired,
+  onClick: func.isRequired,
+  children: any
 }
 
 export default Button
